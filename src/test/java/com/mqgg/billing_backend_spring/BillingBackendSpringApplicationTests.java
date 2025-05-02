e53956b59f2b6ef5a9b5bf1652c8907ef0fea163
Miqueas Garcia <mqggonzalez@gmail.com>
2025-05-02 12:35:21 +0100

    first commit

---
+++ src/test/java/com/mqgg/billing_backend_spring/BillingBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqgg.billing_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
